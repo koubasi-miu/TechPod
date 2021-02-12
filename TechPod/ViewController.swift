da7d7c661c20cfcda01bdff482c2006732c39a8e
Fujii miu <m>
2021-02-12 20:32:26 +0900

    テックポッド　途中

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDateSource {
 
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var tabel: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.dateSource = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return 10
+    }
+    
+    func tableVieew(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Call")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+        
+    }
+    
     }
 
 
-}
+
 
